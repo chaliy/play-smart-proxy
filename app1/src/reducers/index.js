4f13cfe67ec8ed5877b2aa2160719aafa976a9ac
Mike Chaliy <mike@chaliy.name>
2016-08-26 19:29:03 +0300

    Added sceleton of the app1

---
+++ app1/src/reducers/index.js
@@ -0,0 +1,6 @@
+/* Combine all available reducers to a single root reducer.
+ */
+import { combineReducers } from 'redux';
+/* Populated by react-webpack-redux:reducer */
+const reducers = {};
+module.exports = combineReducers(reducers);
